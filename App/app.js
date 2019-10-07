b11ec155871800fe8f135e465e107588442f8bb1
Keith-JK <keithcjk123@gmail.com>
2019-10-08 01:38:26 +0800

    Added Adi's stuff

--- App/app.js
+++ App/app.js
@@ -4,11 +4,34 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+/* --- V7: Using dotenv     --- */
+require('dotenv').config();
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+/* --- V2: Adding Web Pages --- */
+var aboutRouter = require('./routes/about');
+/* ---------------------------- */
 
+/* --- V3: Basic Template   --- */
+var tableRouter = require('./routes/table');
+var loopsRouter = require('./routes/loops');
+/* ---------------------------- */
+
+/* --- V4: Database Connect --- */
+var selectRouter = require('./routes/select');
+/* ---------------------------- */
+
+/* --- V5: Adding Forms     --- */
+var formsRouter = require('./routes/forms');
+/* ---------------------------- */
+
+/* --- V6: Modify Database  --- */
+var insertRouter = require('./routes/insert');
+/* ---------------------------- */
+
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +45,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+/* --- V2: Adding Web Pages --- */
+app.use('/about', aboutRouter);
+/* ---------------------------- */
+
+/* --- V3: Basic Template   --- */
+app.use('/table', tableRouter);
+app.use('/loops', loopsRouter);
+/* ---------------------------- */
+
+/* --- V4: Database Connect --- */
+app.use('/select', selectRouter);
+/* ---------------------------- */
+
+/* --- V5: Adding Forms     --- */
+app.use('/forms', formsRouter);
+/* ---------------------------- */
+
+/* --- V6: Modify Database  --- */
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/insert', insertRouter);
+/* ---------------------------- */
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,5 +85,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// uncomment this if using nodemon / node, else if node bin/www, leave it commented
+/* var server = app.listen(3000, function() {
+  console.log('Listening on port %d', server.address().port);
+}); */
 
 module.exports = app;
